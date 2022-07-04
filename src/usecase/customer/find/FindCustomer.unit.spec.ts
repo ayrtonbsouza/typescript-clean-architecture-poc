b4ff9d355e10bf114395cde52b4fddab153f31be
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-04 18:08:31 -0300

    [:white_check_mark:] testing exception on find customer use case

--- src/usecase/customer/find/FindCustomer.unit.spec.ts
+++ src/usecase/customer/find/FindCustomer.unit.spec.ts
@@ -39,4 +39,18 @@ describe('[Unit] FindCustomer UseCase', () => {
 
     expect(output).toEqual(expected);
   });
+
+  it('should receive an error when the customer is not found', () => {
+    const customerRepository = MockRepository();
+    customerRepository.find.mockImplementation(() => {
+      throw new Error('Customer not found');
+    });
+
+    const useCase = new FindCustomerUseCase(customerRepository);
+    const input = {
+      id: '1234567890',
+    };
+
+    expect(() => useCase.execute(input)).rejects.toThrow('Customer not found');
+  });
 });
