5a9a9b0856579a399b3d6c19d5ac96f0f5ec259c
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-06 00:53:43 -0300

    [:test_tube:] initital customer e2e test development

---
+++ src/infraestructure/api/__tests__/Customer.e2e.spec.ts
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import { app, sequelize } from '../express';
+
+describe('[E2E] Customer', () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it('should be able to create a customer', async () => {
+    const response = await request(app)
+      .post('/customers')
+      .send({
+        name: 'John Doe',
+        address: {
+          street: 'Main St',
+          number: 123,
+          zip: '12345',
+          city: 'New York',
+          state: 'NY',
+        },
+      });
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.name).toBe('John Doe');
+    expect(response.body.address.street).toBe('Main St');
+    expect(response.body.address.number).toBe(123);
+    expect(response.body.address.zip).toBe('12345');
+    expect(response.body.address.city).toBe('New York');
+    expect(response.body.address.state).toBe('NY');
+  });
+});
